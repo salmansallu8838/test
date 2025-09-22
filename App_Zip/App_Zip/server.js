d01afa15a2c772206ba7bd6f6ce73cff147d691c
salmansallu8838 <143182716+salmansallu8838@users.noreply.github.com>
2025-09-22 16:23:42 +0530

    Delete App_Zip/App_Zip directory

--- App_Zip/App_Zip/server.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/", (_, res) => res.send("Hello from Azure App Service 👋"));
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on ${port}`));
